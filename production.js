c8588e28b6583cccebbce81972a4e2bd81ea01be
Lovell Felix <lovell.felix@gmail.com>
2014-08-13 22:53:54 -0500

    Added forever production server file

---
+++ production.js
@@ -0,0 +1,28 @@
+var deployd = require('deployd');
+
+var server = deployd({
+  port: process.env.PORT || 5000,
+  env: 'production',
+  db: {
+    host: '127.0.0.1',
+    port: 27017,
+    name: 'timecapsule',
+    credentials: {
+      username: 'tc',
+      password: '$kynet'
+    }
+  }
+});
+
+server.listen();
+
+server.on('listening', function() {
+  console.log("Server is listening");
+});
+
+server.on('error', function(err) {
+  console.error(err);
+  process.nextTick(function() { // Give the server a chance to return an error
+    process.exit();
+  });
+});
