67a54a5a85ac71d6d13bc19afe33079ac58f1dd3
Lovell Felix <lovell.felix@gmail.com>
2014-08-18 00:27:58 -0500

    Implemented a working copy of file upload

---
+++ public/js/upload.js
@@ -0,0 +1,67 @@
+
+var files = [];
+
+$('.alert-success').hide();
+
+var uploadFiles = function() {
+
+    var fd = new FormData()
+    for (var i in files) {
+        fd.append("uploadedFile", files[i])
+    }
+
+    var subdir = $('#subdir').val();
+    var comments = $('#comments').val();
+    var uniqueFilename = $('#uniqueFilename').prop('checked');
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', '/uploads?subdir=' + subdir + '&comments=' + comments + '&uniqueFilename=' + uniqueFilename);
+    xhr.onload = function() {
+        var response = JSON.parse(this.responseText);
+        console.log(response);
+        $('.alert-success').append("Upload successful!<br />");
+        for (var index in response) {
+           appendUploadedFileToTable(response[index]);
+        }
+    };
+    xhr.onerror = function(err) {
+        alert("Error: ", err);
+    }
+    xhr.send(fd);
+
+};
+
+var appendUploadedFileToTable = function(file) {
+    $('#result tr:last').after(
+            "<tr><td><a href='s3/" + file.subdir + "/" + file.filename + "'>" + file.filename + "</a></td>" +
+            "<td>" + file.subdir + "</td>" +
+            "<td>" + file.comments + "</td>" +
+            "<td>" + file.filesize + "</td>" +
+            "<td>" + file.originalFilename + "</td>" +
+            "<td>" + new Date(file.creationDate).toLocaleString() + "</td>" +
+            "<td><button class='btn btn-default btn-sm' " +
+            "onClick='deleteFile(this, &quot;" + file.id + "&quot;)'>Delete</button></td></tr>");
+
+}
+
+var setFiles = function(element) {
+  console.log('files:', element.files);
+  // Turn the FileList object into an Array
+    files = [];
+    for (var i = 0; i < element.files.length; i++) {
+      files.push(element.files[i]);
+    }
+};
+
+dpd.uploads.get(function(data, statusCode, headers, config) {
+    for(var index in data) {
+        appendUploadedFileToTable(data[index]);
+    }
+});
+
+var deleteFile = function(element, id) {
+    $(element).closest('tr').remove();
+    dpd.uploads.del(id, function(data, status) {
+        $('.alert-success').show();
+        $('.alert-success').append("File removed!");
+    })
+}
